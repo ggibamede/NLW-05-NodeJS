73d0e92410b0afabe5dedc6d1a35e7576a7552a3
ggibamede <gibamedeirosgc@gmail.com>
2021-04-20 16:56:39 -0300

    feat: Dia 2
    - Criação do Controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,8 @@
 import { Router} from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from './repositories/SettingsRepository';
-
+import { SettingsController } from './controllers/SettingsController';
 const routes = Router();
 
-routes.post("/settings", async (req, res)=>{
-    const { chat , username } = req.body;
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const settings = settingsRepository.create({
-        chat,
-        username
-    })
-    await settingsRepository.save(settings);
-
-    return res.json(settings)
-
-})
+const settingsController = new SettingsController();
+routes.post("/settings", settingsController.create)
 
 export {routes}
