6d6c7b49408840ab964ff17761a232f9bff82b95
ggibamede <gibamedeirosgc@gmail.com>
2021-04-21 18:17:51 -0300

    feat: Dia 3
    - Estrutura dos connections
    - Estrutura das messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,16 @@
 import { Router} from 'express';
+import { MessagesController } from './controllers/MessagesController';
 import { SettingsController } from './controllers/SettingsController';
+import { UsersController } from './controllers/UsersController';
 const routes = Router();
 
 const settingsController = new SettingsController();
-routes.post("/settings", settingsController.create)
+const usersController = new UsersController();
+const messagesController = new MessagesController();
+routes.post("/settings", settingsController.create);
+routes.post("/users", usersController.create);
+
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
 
 export {routes}
---
+++ src/services/UsersService.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository"
+
+class UsersService{
+    private usersRepository : Repository<User>;
+
+    constructor(){
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+    async create(email : string){ 
+        //Verificar se o usuario ja existe
+        const userExists = await this.usersRepository.findOne({
+            email
+        })
+        //Se existir retornar o user
+        if(userExists){
+            return userExists;
+        }
+
+        const user = this.usersRepository.create({
+            email
+        });
+
+        await this.usersRepository.save(user);
+
+        //Se não existir salvar no banco de dados
+        return user;
+    }
+}
+
+export { UsersService}
